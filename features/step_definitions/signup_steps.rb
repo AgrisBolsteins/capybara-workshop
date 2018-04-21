bb73c44617a4b9e39607d13c1123b40cd425e4f2
Agris Bolšteins <agris.bolsteins@testdevlab.com>
2018-04-21 08:59:16 +0300

    Code refactoring

--- features/step_definitions/signup_steps.rb
+++ features/step_definitions/signup_steps.rb
@@ -1,26 +1,39 @@
 When(/^I am on Appimation home page/) do
   visit('/')
-########## xPath elements ##########
+
 # try now button
 find(:xpath, "//section/descendant::button[contains(text(), 'Try now')]")
+find(:css, "#start_button")
 # sign up button
 find(:xpath, "//header/descendant::button[@id='signup-b']")
+find(:css, "#signup-b")
 # contact us label
 find(:xpath, "//h2[contains(text(), 'Contact Us')]")
+find(:css, "#cta > h2")
 # contact us name input
 find(:xpath, "//div/descendant::input[@id='name']")
+find(:css, "#name")
 # contact us email input
 find(:xpath, "//div/descendant::input[@id='email']")
+find(:css, "#email")
 # contact us message input
 find(:xpath, "//textarea[@id='contactus-message']")
+find(:css, "#contactus-message")
 # contact us send button
 find(:xpath, "//div/descendant::input[@id='contactus-button']")
+find(:css, "#contactus-button")
 # facebook logo
 find(:xpath, "//footer/descendant::a[@class='icon fa-facebook']")
+find(:css, ".icon")
 # all feature sections in a list
   puts " "
   puts "Feature Sections Element list by xPath: "
 all(:xpath, "//section[@class='box special features']/descendant::*").each do |element|
+  puts element
+end
+  puts " "
+  puts "Feature Sections Element list by CSS: "
+all(:css, "section.box *").each do |element|
   puts element
 end
   puts " "
@@ -30,31 +43,8 @@ all(:xpath, "//h3[contains(text(), 'Chain requests with reusable data')]/parent:
   puts el
 end
   puts " "
-########## CSS elements ##########
-# try now button
-find(:css, "#start_button")
-# sign up button
-find(:css, "#signup-b")
-# contact us label
-find(:css, "#cta > h2")
-# contact us name input
-find(:css, "#name")
-# contact us email input
-find(:css, "#email")
-# contact us message input
-find(:css, "#contactus-message")
-# contact us send button
-find(:css, "#contactus-button")
-# facebook logo
-find(:css, ".icon")
-# all feature sections in a list
-  puts "Feature Sections Element list by CSS: "
-all(:css, "section.box *").each do |element|
-  puts element
-end
-  puts " "
-# Chain requests with reusable data image
-  puts "Chain requests with reusable data image by xPath: "
+
+  puts "Chain requests with reusable data image by CSS: "
 all(:css, "div.features-row:nth-child(1) > section:nth-child(1) > img:nth-child(1)").each do |element|
   puts element
 end
